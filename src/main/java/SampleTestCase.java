609d9b46a7e53e536b0f514640c0515b480bdc03
s.garmaev <garmsayan@gmail.com>
2017-08-18 19:25:17 +0300

    First commit

--- src/main/java/SampleTestCase.java
+++ src/main/java/SampleTestCase.java
@@ -2,9 +2,21 @@ import com.jayway.restassured.response.Response;
 import controllers.UserController;
 import models.User;
 
+import org.apache.http.Consts;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.web.client.RestTemplate;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 import static com.jayway.restassured.RestAssured.given;
@@ -17,6 +29,52 @@ public class SampleTestCase {
     @Before
     public void setUp() {
         userController = new UserController();
+    }
+
+    @Test
+    public void login() throws IOException {
+
+        HttpClient client = new DefaultHttpClient();
+
+        HttpPost loginRequest = new HttpPost("https://staging.ict4apps.aimprosoft.com/welcome/-/login/login?saveLastPath=false");
+        List<NameValuePair> credentials = new ArrayList<NameValuePair>();
+        credentials.add(new BasicNameValuePair("_58_login", " test@liferay.com"));
+        credentials.add(new BasicNameValuePair("_58_password", "test-ict4apps!"));
+
+        loginRequest.setEntity(new UrlEncodedFormEntity(credentials, Consts.UTF_8));
+        HttpResponse response = client.execute(loginRequest);
+        System.out.println(response.getStatusLine().getStatusCode());
+    }
+
+    @Test
+    public void checkAimChat(){
+        Response login_endpoint = given()
+                .parameter("grant_type","password")
+                .parameter("type","LDAP")
+                .parameter("client_id","aimprosoft_chat")
+                .parameter("client_secret","aimprosoft_chat_secret")
+                .parameter("username","s.garmaev")
+                .parameter("password","Rfhfylfitkm")
+                .parameter("Connection","keep-alive")
+                .post("https://chat.aimprosoft.com/oauth/token")
+                .then().extract().response();
+        String authToken = login_endpoint.jsonPath().getJsonObject("access_token");
+
+        login_endpoint = given()
+                .parameter("message","99999999999999999988888888888899999999")
+                .parameter("Host","chat.aimprosoft.com")
+                .parameter("Accept","*/*")
+                .parameter("isSystem","false")
+                .parameter("systemMessageType","PUBLIC")
+                .parameter("Content-Type","application/json")
+                .parameter("Accept-Encoding","gzip, deflate, br")
+                .parameter("Authorization", "bearer "+authToken)
+                .parameter("Connection","keep-alive")
+                .post("https://chat.aimprosoft.com/api/rooms/5870/histories")
+                .then().extract().response();
+
+
+        int i=0;
     }
 
     @Test
@@ -38,12 +96,6 @@ public class SampleTestCase {
         int i=0;
 
     }
-    //_3_formDate	"1503048151679"
-    //_3_cur	"1"
-    //_3_format	""
-    //_3_keywords	"apple"
-    //_3_selected_scope	"product"
-    //_3_documentsSearchContainerPrimaryKeys	"PRODUCT_PORTLET_ECOMMERCE_ENTITY_id_327,PRODUCT_PORTLET_ECOMMERCE_ENTITY_id_331,PRODUCT_PORTLET_ECOMMERCE_ENTITY_id_325"
 
     @Test
     public void testUserInfoResponse() {
